608f2b09918392a1af55b486a1c799a6f908658d
MyGollyGosh <lachlan.hooker@hotmail.com>
2024-11-13 15:16:09 +0000

    add add function with unit tests

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -7,3 +7,8 @@ class UserRepository:
     def all(self):
         rows = self._connection.execute('SELECT * FROM users;')
         return [User(row['username'], row['email'], row['password_hash'], row['id']) for row in rows]
+    
+    def add(self, username, email, password) -> str:
+        user = User(username, email, password)
+        self._connection.execute('INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)', [username, email, password])
+        return f"{user} was successfully created"
--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -1,12 +1,12 @@
 from lib.user import User
 from lib.user_repository import UserRepository
+import pytest
 
 
 '''
 When I call #all
 I get a list of every user
 '''
-
 def test_all_returns_every_user(db_connection):
     db_connection.seed('seeds/task_seeds.sql')
     repo = UserRepository(db_connection)
@@ -14,3 +14,65 @@ def test_all_returns_every_user(db_connection):
         User('johndoe', 'johndoe@example.com', 'Password!1', 1),
         User('janedoe', 'janedoe@example.com', 'Password!2', 2)
         ]
+
+'''
+Given a username, email and password as a User object
+#add puts an extra user in the DB
+'''
+def test_add_with_correct_details_adds_new_user(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = UserRepository(db_connection)
+    repo.add('jimdoe', 'jimdoe@example.com', 'Password!3')
+    assert repo.all() == [
+        User('johndoe', 'johndoe@example.com', 'Password!1', 1),
+        User('janedoe', 'janedoe@example.com', 'Password!2', 2),
+        User('jimdoe', 'jimdoe@example.com', 'Password!3', 3)
+        ]
+
+'''
+Given an invalid username
+#add returns an error and nothing is added to the DB
+'''
+def test_invalid_username_returns_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = UserRepository(db_connection)
+    with pytest.raises(ValueError) as e:
+        repo.add('', 'jimdoe@example.com', 'Password!3')
+    assert str(e.value) == 'Username cannot be empty'
+    assert repo.all() == [
+        User('johndoe', 'johndoe@example.com', 'Password!1', 1),
+        User('janedoe', 'janedoe@example.com', 'Password!2', 2)
+        ]
+
+
+'''
+Given an invalid email
+#add returns an error and nothing is added to the DB
+'''
+def test_invalid_password_returns_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = UserRepository(db_connection)
+    with pytest.raises(ValueError) as e:
+        repo.add('jimdoe', 'jimdoeexample.com', 'Password!3')
+    assert str(e.value) == 'Invalid email'
+    assert repo.all() == [
+        User('johndoe', 'johndoe@example.com', 'Password!1', 1),
+        User('janedoe', 'janedoe@example.com', 'Password!2', 2)
+        ]
+
+'''
+Given an invalid password
+#add returns an error and nothing is added to the DB
+'''
+def test_invalid_password_returns_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = UserRepository(db_connection)
+    with pytest.raises(ValueError) as e:
+        repo.add('jimdoe', 'jimdoe@example.com', 'Password3')
+    assert str(e.value) == 'Invalid password'
+    assert repo.all() == [
+        User('johndoe', 'johndoe@example.com', 'Password!1', 1),
+        User('janedoe', 'janedoe@example.com', 'Password!2', 2)
+        ]
+
+
