1f96ee7ebd5ed69df86f5fe0e5c7f025a02e79bf
MyGollyGosh <lachlan.hooker@hotmail.com>
2024-11-20 15:10:39 +0000

    add all_tasks and unit tests + get_by_task_id and unit tests

--- lib/task_repository.py
+++ lib/task_repository.py
@@ -6,5 +6,12 @@ class TaskRepository:
 
     def all_tasks(self) -> list:
         rows = self._connection.execute('SELECT * FROM tasks;')
-        print(type(rows[0]['date_added']))
         return [Task(row['user_id'], row['description'], row['due_date'], row['date_added'], row['priority'], row['status']) for row in rows]
+    
+    def get_by_task_id(self, id) -> object:
+        try:
+            task = self._connection.execute('SELECT * FROM tasks WHERE id = %s', (id,))[0]
+        except:
+            raise IndexError(f'No task with id {id} found')
+        else:
+            return Task(task['user_id'], task['description'], task['due_date'], task['date_added'], task['priority'], task['status'])
--- tests/test_task_repository.py
+++ tests/test_task_repository.py
@@ -1,5 +1,8 @@
 from lib.task_repository import TaskRepository
 from lib.task import Task
+from datetime import date
+import pytest
+
 
 '''
 when I call #all_tasks
@@ -10,8 +13,44 @@ def test_all_tasks_returns_list(db_connection):
     repo = TaskRepository(db_connection)
     tasks = repo.all_tasks()
     assert tasks == [
-        Task(1, 'Complete Flask web app project', '2024-11-15', tasks[0].timestamp, 3, 'pending'),
-        Task(1, 'Prepare presentation for project', '2024-11-10', tasks[1].timestamp, 2, 'in-progress'),
-        Task(2, 'Review portfolio projects', '2024-11-20', tasks[2].timestap, 1, 'pending'),
-        Task((2, 'Update resume', '2024-11-12', tasks[3].timestamp, 3, 'completed'))
+        Task(1, 'Complete Flask web app project', date(2024, 11, 15), tasks[0].date_added, 3, 'pending'),
+        Task(1, 'Prepare presentation for project', date(2024, 11, 10), tasks[1].date_added, 2, 'in-progress'),
+        Task(2, 'Review portfolio projects', date(2024, 11, 20), tasks[2].date_added, 1, 'pending'),
+        Task(2, 'Update resume', date(2024, 11, 12), tasks[3].date_added, 3, 'completed')
     ]
+#In future, worth adding in more robust testing for date_added to ensure higher quality. Perhaps a check to ensure timestamp is recent.
+
+'''
+given an (task) id
+#get_by_task_id returns the Task object with that id
+'''
+def test_get_by_task_id_gives_task_object(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = TaskRepository(db_connection)
+    task = repo.get_by_task_id(3)
+    assert task == Task(2, 'Review portfolio projects', date(2024, 11, 20), task.date_added, 1, 'pending')
+
+'''
+Given an (task) id that is not in the DB
+#get_by_task raises an error
+'''
+def test_invalid_task_id_gives_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = TaskRepository(db_connection)
+    with pytest.raises(IndexError) as e:
+        repo.get_by_task_id(100)
+    assert str(e.value) == 'No task with id 100 found'
+
+def test_different_invalid_task_id_gives_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = TaskRepository(db_connection)
+    with pytest.raises(IndexError) as e:
+        repo.get_by_task_id(-100)
+    assert str(e.value) == 'No task with id -100 found'
+
+def test_again_different_invalid_task_id_gives_error(db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    repo = TaskRepository(db_connection)
+    with pytest.raises(IndexError) as e:
+        repo.get_by_task_id('hi')
+    assert str(e.value) == 'No task with id hi found'
