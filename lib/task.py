5bef89288a7cab8d6cc8b58d3600da54400772ba
MyGollyGosh <lachlan.hooker@hotmail.com>
2024-11-25 21:34:33 +0000

    add update task and unit tests. Also move validation logic out of Task class to be used elsewhere in the class

--- lib/task.py
+++ lib/task.py
@@ -4,17 +4,17 @@ class Task:
     def __init__(self, user_id, description, due_date, date_added, priority, status, id=None):
         self.id = id
         self.user_id = user_id
-        self.check_valid_user_id(user_id)
+        check_valid_user_id(user_id)
         self.description = description
-        self.check_valid_description(description)
+        check_valid_description(description)
         self.due_date = due_date
-        self.check_valid_due_date(due_date)
+        check_valid_due_date(due_date)
         self.date_added = date_added
-        self.check_valid_date_added(date_added)
+        check_valid_date_added(date_added)
         self.priority = priority
-        self.check_valid_priority(priority)
+        check_valid_priority(priority)
         self.status = status
-        self.check_valid_status(status)
+        check_valid_status(status)
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
@@ -22,34 +22,36 @@ class Task:
     def __repr__(self):
         return f"Task({self.user_id}, {self.description}, {self.due_date}, {self.date_added}, {self.priority}, {self.status}, {self.id})"
 
-    def check_valid_user_id(self, user_id) -> None:
-        if type(user_id) != int:
-            raise TypeError('Ensure valid user id (int)')
-
-        
-    def check_valid_description(self, description) -> None:
-        if type(description) != str:
-            raise TypeError('Description must be a string')
-        if len(description) < 3:
-            raise ValueError('Invalid description')
-
-        
-    def check_valid_due_date(self, due_date) -> None:
-        if not isinstance(due_date, date):
-            raise TypeError('Ensure valid due_date (date(YYYY, MM, DD))')
-
-        
-    def check_valid_date_added(self, date_added) -> None:
-        if not isinstance(date_added, datetime):
-            raise TypeError('Ensure valid date_added (datetime(YYYY,MM,DD, HH,MI,SS))')
 
 
-    def check_valid_priority(self, priority) -> None:
-        if type(priority) != int:
-            raise TypeError('Ensure valid priority (int 1-5)')
+def check_valid_user_id(user_id) -> None:
+    if type(user_id) != int:
+        raise TypeError('Ensure valid user id (int)')
+
+    
+def check_valid_description(description) -> None:
+    if type(description) != str:
+        raise TypeError('Description must be a string')
+    if len(description) < 3:
+        raise ValueError('Invalid description')
+
+    
+def check_valid_due_date(due_date) -> None:
+    if not isinstance(due_date, date):
+        raise TypeError('Ensure valid due_date (date(YYYY, MM, DD))')
+
+    
+def check_valid_date_added(date_added) -> None:
+    if not isinstance(date_added, datetime):
+        raise TypeError('Ensure valid date_added (datetime(YYYY,MM,DD, HH,MI,SS))')
 
 
-    def check_valid_status(self, status) -> None:
-        if type(status) != str:
-            raise TypeError('Ensure valid status (str)')
+def check_valid_priority(priority) -> None:
+    if type(priority) != int:
+        raise TypeError('Ensure valid priority (int 1-5)')
+
+
+def check_valid_status(status) -> None:
+    if type(status) != str:
+        raise TypeError('Ensure valid status (str)')
     
