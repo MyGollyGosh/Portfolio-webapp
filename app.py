f6501b7af69de6cf716d924177a7837de540c8b4
MyGollyGosh <lachlan.hooker@hotmail.com>
2024-12-19 16:48:37 +0000

    adjust login_manager to be fully functional and handle checking for each user. Secret key still needs to be handled as an actual secret

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask_login import LoginManager
 from lib.user import User
+from lib.user_repository import UserRepository
+from lib.database_connection import get_flask_database_connection
 from routes.index import get_index_routes
 from routes.home import get_home_routes
 from routes.log_in import get_log_in_routes
@@ -16,11 +18,18 @@ login_manager.init_app(app)
 login_manager.login_view = 'log_in'
 
 @login_manager.user_loader
-def load_user():
+def load_user(user_id):
     try:
-        return User.get_id()
-    except:
+        connection = get_flask_database_connection(app)
+        repo = UserRepository(connection)
+        user_id = int(user_id)
+        user = repo.get_by_id(user_id)
+        if user is None:
+            return None
+        return user
+    except ValueError:
         return None
+
 
 get_home_routes(app)
 get_index_routes(app)
