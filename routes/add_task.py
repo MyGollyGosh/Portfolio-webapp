904f56bcd045682beabdca4d1329aeb1fd89104d
MyGollyGosh <lachlan.hooker@hotmail.com>
2025-03-20 13:37:32 +0000

    remove bad ai code. Add new page for /add-task. Make first test to check if task is added. Minor formatting changes

--- routes/add_task.py
+++ routes/add_task.py
@@ -1,7 +1,23 @@
-from flask import render_template
+from flask import render_template, request
+from flask_login import current_user
+from lib.task_repository import TaskRepository
+from lib.database_connection import get_flask_database_connection
 
 def get_add_task_routes(app):
 
-    @app.route('/add-task', methods=['GET'])
+    @app.route('/add-task', methods=['GET', 'POST'])
     def get_add_task():
-        return render_template('add_task.html')
+        print(request.method)
+        if request.method == 'GET':
+            return render_template('add_task.html')
+        
+        if request.method == 'POST':
+            connection = get_flask_database_connection(app)
+            repo = TaskRepository(connection)
+            description = request.form['description']
+            due_date = request.form['due-date']
+            priority = request.form['priority']
+            repo.add_task(current_user.id, description, due_date, priority, 'pending')
+            print('21')
+            print(repo.all_tasks())
+            
--- tests/test_add_task.py
+++ tests/test_add_task.py
@@ -1,4 +1,6 @@
-from playwright.sync_api import expect
+from lib.task_repository import TaskRepository
+from lib.task import Task
+from datetime import date
 
 '''
 test /add-tasks gets a 200 response
@@ -28,6 +30,27 @@ def test_required_information_boxes_exist_on_page(test_web_address, page, db_con
     description_input = page.locator('input#description')
     due_date_input = page.locator('input#due-date')
     priority_input = page.locator('input#priority')
+    post_button = page.locator('#submit-task')
     assert description_input.count() == 1
     assert due_date_input.count() == 1
     assert priority_input.count() == 1
+
+'''
+When I hit submit with valid data, a task is added to my account
+'''
+def test_valid_submit_adds_task_to_account(test_web_address, page, db_connection):
+    db_connection.seed('seeds/task_seeds.sql')
+    page.goto(f'http://{test_web_address}/log-in')
+    page.fill('input[name=uname]', 'johndoe')
+    page.fill('input[name=pwd]', 'Password!1')
+    page.locator('#log-in').click()
+    assert page.url == f'http://{test_web_address}/home'
+    page.goto(f'http://{test_web_address}/add-task')
+    assert page.url == f'http://{test_web_address}/add-task'
+    page.fill('input[name=description]', 'First description added')
+    page.fill('input[name=due-date]', '2026-01-01')
+    page.fill('input[name=priority]', '2')
+    page.locator('#submit-task').click()
+    repo = TaskRepository(db_connection)
+    task = repo.get_by_task_id(5)
+    assert task == Task(1, 'First description added', date(2026, 1, 1), task.date_added, 2, 'pending')
